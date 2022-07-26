37800ea30f37e806b4efa56b740787d2751f76a3
suns-a <100550821+suns-a@users.noreply.github.com>
2022-07-26 16:20:03 +0900

    LivewirePropertyBinding

---
+++ laravel8livewire/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Livewire\Form;
+use App\Http\Livewire\Home;
+use App\Http\Livewire\Post;
+use App\Http\Livewire\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/post', Post::class);
+
+Route::get('/user', User::class);
+
+Route::get('/home/{name?}', Home::class);
+
+Route::get('/form', Form::class);
